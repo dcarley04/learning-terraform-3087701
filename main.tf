a448ad92c390cc3fbb816fb537b30bbf8c7389a7
dcarley04 <darrellcarley2@gmail.com>
2025-09-23 12:42:56 -0400

    Create default vpc, added security groups & rules.

--- main.tf
+++ main.tf
@@ -14,11 +14,53 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default-vpc" {
+  default = true
+}
+
 resource "aws_instance" "web_blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_blog_sg.id]
+  
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
+
+resource "aws_security_group" "web_blog_sg" {
+  name        = "web_blog_sg"
+  description = "Allow http and https in. Allow everything out.
 
   tags = {
-    Name = "HelloWorld"
+    Terraform = true
   }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_blog_sgr_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_blog_sg.id
+}
+
+resource "aws_security_group_rule" "web_blog_sgr_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_blog_sg.id
+}
+
+resource "aws_security_group_rule" "web_blog_sgr_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_blog_sg.id
 }
