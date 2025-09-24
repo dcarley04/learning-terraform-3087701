7a8d819cbcbc7c910c87cf35651d3060a835bdfb
dcarley04 <darrellcarley2@gmail.com>
2025-09-24 15:31:00 -0400

    Added map_public_ip under "aws_instance" for test#1.

--- main.tf
+++ main.tf
@@ -34,10 +34,11 @@ module "blog_vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id             = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  ami                     = data.aws_ami.app_ami.id
+  instance_type           = var.instance_type
+  subnet_id               = module.blog_vpc.public_subnets[0]
+  map_public_ip_on_launch = true
+  vpc_security_group_ids  = [module.blog_sg.security_group_id]
   
   tags = {
     Name = "Learning Terraform"
@@ -55,38 +56,3 @@ module "blog_sg" {
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
-
-#resource "aws_security_group" "web_blog_sg" {
-#  name        = "web_blog_sg"
-#  tags        = {
-#    Terraform = "true"
-#  }
-#  vpc_id = data.aws_vpc.default_vpc.id
-#}
-
-#resource "aws_security_group_rule" "web_blog_sgr_http_in" {
-#  type        = "ingress"
-#  from_port   = 80
-#  to_port     = 80
-#  protocol    = "tcp"
-#  cidr_blocks = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.web_blog_sg.id
-#}
-
-#resource "aws_security_group_rule" "web_blog_sgr_https_in" {
-#  type        = "ingress"
-# from_port   = 443
-#  to_port     = 443
-#  protocol    = "tcp"
-#  cidr_blocks = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.web_blog_sg.id
-#}
-
-#resource "aws_security_group_rule" "web_blog_sgr_everything_out" {
-#  type        = "egress"
-#  from_port   = 0
-#  to_port     = 0
-#  protocol    = "-1"
-#  cidr_blocks = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.web_blog_sg.id
-#}
