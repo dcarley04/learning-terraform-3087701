0b46b907ac340b74fdfe44a3e13e07dce7d50701
dcarley04 <darrellcarley2@gmail.com>
2025-09-24 14:02:27 -0400

    Baseling to end of 4.1 Config.

--- main.tf
+++ main.tf
@@ -33,27 +33,25 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "web_blog" {
+resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.web_blog_sg_new.security_group_id]
+  vpc_security_group_ids = [module.web_blog_sg.security_group_id]
   
   tags = {
     Name = "Learning Terraform"
   }
 }
 
-module "web_blog_sg_new" {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
-  name = "web_blog_sg_new"
 
   vpc_id = module.blog_vpc.vpc_id
-
+  name   = "blog"
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
